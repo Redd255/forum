3d6adf827ec1a1fb6869205f21ca66ae6f0642ee
Your Name <you@example.com>
2025-01-13 17:10:20 +0100

    forum

--- main.go
+++ main.go
@@ -6,16 +6,15 @@ import (
 	"log"
 	"net/http"
 
-	myserver "test/src" 
+	myserver "test/src"
 
-	_ "github.com/mattn/go-sqlite3" 
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var db *sql.DB
 
 func init() {
 	var err error
-	// Connect to the SQLite database
 	db, err = sql.Open("sqlite3", "./my.db")
 	if err != nil {
 		log.Fatal(err)
@@ -33,12 +32,10 @@ func init() {
 	if err != nil {
 		log.Fatal("Failed to create table:", err)
 	}
-
 }
 
 func main() {
 	http.HandleFunc("/", myserver.HomePage)
-	http.HandleFunc("/store", myserver.StorData)
 
 	fmt.Println("Server running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
--- src/servers.go
+++ src/servers.go
@@ -5,24 +5,14 @@ import (
 	"net/http"
 )
 
-var db *sql.DB 
+var db *sql.DB
 
 func InitDatabase(database *sql.DB) {
 	db = database
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
-}
-
-func StorData(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-
 	username := r.FormValue("fname")
-
 	insertQuery := "INSERT INTO users (username) VALUES (?)"
 	_, err := db.Exec(insertQuery, username)
 	if err != nil {
