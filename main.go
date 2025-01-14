e991a1768ac6f518fbfb5da07a93edfef32ab9c3
Your Name <you@example.com>
2025-01-14 16:46:12 +0100

    forum

--- main.go
+++ main.go
@@ -24,18 +24,27 @@ func init() {
 	myserver.InitDatabase(db)
 
 	createTableQuery := `
-	CREATE TABLE IF NOT EXISTS users (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		username 
+	DROP TABLE IF EXISTS users;
+	CREATE TABLE users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT, -- Unique ID for each user
+		name TEXT NOT NULL,                   -- User's name
+		email TEXT NOT NULL UNIQUE,           -- User's email (must be unique)
+		age INTEGER,                          -- User's age
+		pass TEXT NOT NULL
 	);`
+
 	_, err = db.Exec(createTableQuery)
 	if err != nil {
-		log.Fatal("Failed to create table:", err)
+		log.Fatal("Failed to recreate table:", err)
 	}
 }
 
 func main() {
-	http.HandleFunc("/", myserver.HomePage)
+	http.HandleFunc("/", myserver.SingUp)
+	http.HandleFunc("/HomePage", myserver.HomePage)
+	http.HandleFunc("/SingIn", myserver.SingIn)
+	http.HandleFunc("/SingIn2", myserver.SingIn2)
+
 
 	fmt.Println("Server running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
--- src/servers.go
+++ src/servers.go
@@ -2,6 +2,7 @@ package myserver
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 )
 
@@ -11,13 +12,43 @@ func InitDatabase(database *sql.DB) {
 	db = database
 }
 
-func HomePage(w http.ResponseWriter, r *http.Request) {
+func SingUp(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "singup.html")
+}
+
+func SingIn(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
 	username := r.FormValue("fname")
-	insertQuery := "INSERT INTO users (username) VALUES (?)"
-	_, err := db.Exec(insertQuery, username)
-	if err != nil {
-		http.Error(w, "Unable to insert data", http.StatusInternalServerError)
+	gmail := r.FormValue("gmail")
+	age := r.FormValue("age")
+	pass := r.FormValue("pass")
+	insertQuery := `INSERT INTO users (name, email, age,pass) VALUES (?, ?, ?, ?);`
+	_, err1 := db.Exec(insertQuery, username, gmail, age, pass)
+	if err1 != nil {
+		// http.Error(w, "Unable to insert data: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
-	http.ServeFile(w, r, "index.html")
+	// var test *sql.Row
+	// err := db.QueryRow("SELECT pass FROM users WHERE username = ?", username).Scan(&test)
+	// if err != nil {
+	// 	fmt.Println("err")
+	// }
+	http.ServeFile(w, r, "singin.html")
+}
+
+func SingIn2(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := r.FormValue("fname")
+	// pass := r.FormValue("pass")
+	var test string
+	err := db.QueryRow("SELECT pass FROM users WHERE name = ?", username).Scan(&test)
+	if err != nil {
+		fmt.Println("err")
+	}
+	fmt.Println(test)
+	http.ServeFile(w, r, "singin2.html")
+}
+
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "homepage.html")
 }
