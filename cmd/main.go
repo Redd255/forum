603127b4d15e49a2fa124a8b6c0e2f2610290b35
yhayyani <youssef16hay@gmail.com>
2025-01-26 05:40:22 +0100

    forum

--- cmd/main.go
+++ cmd/main.go
@@ -5,47 +5,61 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	myserver "test/src"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-
 func init() {
-	var err error
 	db, err := sql.Open("sqlite3", "../database/my.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	createTableQuery := `
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL
-    );`
-	_, err = db.Exec(createTableQuery)
+	// Create tables
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			username TEXT NOT NULL,
+			email TEXT NOT NULL UNIQUE,
+			password TEXT NOT NULL
+		)`)
 	if err != nil {
-		log.Fatal("Failed to create table:", err)
+		log.Fatal("Failed to create users table:", err)
 	}
-	createPostsTableQuery := `
-    CREATE TABLE IF NOT EXISTS posts (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER NOT NULL,
-        content TEXT NOT NULL,
-        FOREIGN KEY(user_id) REFERENCES users(id)
-    );`
-	_, err = db.Exec(createPostsTableQuery)
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS posts (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			user_id INTEGER NOT NULL,
+			content TEXT NOT NULL,
+			FOREIGN KEY(user_id) REFERENCES users(id)
+		)`)
 	if err != nil {
 		log.Fatal("Failed to create posts table:", err)
 	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS sessions (
+			session_id TEXT PRIMARY KEY,
+			user_id INTEGER NOT NULL,
+			expiry DATETIME NOT NULL,
+			FOREIGN KEY(user_id) REFERENCES users(id)
+		)`)
+	if err != nil {
+		log.Fatal("Failed to create sessions table:", err)
+	}
+
 	myserver.InitHandlers(db)
 }
 
 func main() {
+	staticDir := filepath.Join("..", "static")
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(staticDir))))
 	http.HandleFunc("/", myserver.SignUp)
+	http.HandleFunc("/signup", myserver.SignUp)
 	http.HandleFunc("/signin", myserver.SignIn)
 	http.HandleFunc("/homepage", myserver.HomePage)
 
