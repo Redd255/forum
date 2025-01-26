5072befe9befebbc0aeca379d27c904761a44e2d
yhayyani <youssef16hay@gmail.com>
2025-01-26 06:26:05 +0100

    forum

--- cmd/main.go
+++ cmd/main.go
@@ -21,7 +21,7 @@ func init() {
 	_, err = db.Exec(`
 		CREATE TABLE IF NOT EXISTS users (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			username TEXT NOT NULL,
+			username TEXT NOT NULL UNIQUE,
 			email TEXT NOT NULL UNIQUE,
 			password TEXT NOT NULL
 		)`)
